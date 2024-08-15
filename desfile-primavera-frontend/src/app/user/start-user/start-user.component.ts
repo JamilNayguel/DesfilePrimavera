3cab248ecc2c40cfc454367f75a154af0e01db36
JamilNayguel <jamilcastro2001@gmail.com>
2024-08-15 16:05:03 -0400

    create component module and services user

---
+++ desfile-primavera-frontend/src/app/user/start-user/start-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-user.component.html',
+  styleUrl: './start-user.component.css'
+})
+export class StartUserComponent {
+
+}
