d66b90791c067280469aea33422b3ce871a9054a
JamilNayguel <jamilcastro2001@gmail.com>
2024-08-16 09:46:09 -0400

    create component module and services from request

---
+++ desfile-primavera-frontend/src/app/request/list-request/list-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-request.component.html',
+  styleUrl: './list-request.component.css'
+})
+export class ListRequestComponent {
+
+}
