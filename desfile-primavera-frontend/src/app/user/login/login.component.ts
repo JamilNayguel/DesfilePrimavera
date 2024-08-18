272fa0bacba1efccfe4db0bf4aed46385820ceb4
JamilNayguel <jamilcastro2001@gmail.com>
2024-08-18 14:24:36 -0400

    create listRecuestConsultation and login user, config module andand services

--- desfile-primavera-frontend/src/app/user/login/login.component.ts
+++ desfile-primavera-frontend/src/app/user/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user = {
+    Usuario: '',
+    Contrasena: ''
+  };
+  errorMessage:string =''
 
+  constructor(private userService: UserService, private router: Router) {}
+    onSubmit(): void {
+      const {Usuario,Contrasena}=this.user;
+      
+        this.userService.login(Usuario,Contrasena).subscribe(
+          response => {
+            this.router.navigate(['/user/start-user']); // Redirigir al dashboard después del inicio de sesión
+          },
+          error => {
+            this.errorMessage = 'Nombre de usuario o contraseña incorrectos.';
+          }
+        );
+      
+    }
+  
 }
